90bf6dd300c63c76b7ba10745f446420f980de34
Yuliyan Samuilov <YSamuilov@nemetschek.bg>
2020-12-11 12:26:27 +0200

    heroku-added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
-import { NgModule } from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RoleComponent } from './role/role.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{
+  path: "home",
+  loadChildren: () => import('./home/home.module').then(m=>m.HomeModule)
+},
+
+{
+  path: "role",
+  component: RoleComponent
+},
+
+{
+  path: "user",
+  loadChildren: () => import('./user/user.module').then(m=>m.UserModule)
+},
+
+
+{
+  path: "**",
+  component: HomeComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { UserRoleComponent } from './user/user-role/user-role.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: UserListComponent
+  },
+  {
+    path: ":id",
+    component: UserComponent,
+    children:[
+      {
+        path: "details",
+        component: UserDetailsComponent
+      },
+      {
+        path: "role",
+        component: UserRoleComponent
+      },
+
+      {
+        path:"",
+        redirectTo:"details",
+        pathMatch: "full"
+      }
+
+    ]
+  }
+  
+  
+  ];
+
+@NgModule({
+  declarations: [UserListComponent, UserComponent, UserDetailsComponent, UserRoleComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class UserModule { }
